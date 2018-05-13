78456d3579aec315b26d85eeec2870aeef45941b
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2018-05-13 14:04:40 -0700

    add some basic game server routes

--- falcon_server/src/main.rs
+++ falcon_server/src/main.rs
@@ -18,7 +18,20 @@ fn health() -> &'static str {
     "healthy"
 }
 
+#[get("/list")]
+fn list() -> &'static str {
+    ""
+}
 
+#[post("/join/<id>")]
+fn join(id: u32) ->  String {
+    format!("{}", id)
+}
+
+#[post("/leave/<id>")]
+fn leave(id: u32) ->  String {
+    format!("{}", id)
+}
 
 fn main() {
     rocket::ignite().mount("/", routes![index]).launch();
